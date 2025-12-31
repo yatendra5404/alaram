af0cc006a061fbf69b55c76db10095b0607a7845
yatendra5404 <gurjars5404@gmail.com>
2025-12-31 07:59:44 +0000

    CI: add pluginManagement and repositories in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		google()
+		mavenCentral()
+	}
+}
+
+dependencyResolutionManagement {
+	repositories {
+		google()
+		mavenCentral()
+	}
+}
+
 rootProject.name = "alaram"
 include(":app")
